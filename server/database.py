38812c224a6b5bee44334488d2fd239c0de6daa7
Alexander Pustovalov <Pustovalov.al.an@gmail.com>
2024-05-28 02:16:58 +0300

    Еще тесты

--- server/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
-
-config = dotenv_values()
-engene = create_async_engine(config.get("db_url"))
-
-session = async_sessionmaker(engene, expire_on_commit=False)
